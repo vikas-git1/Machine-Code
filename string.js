7874b50df70e9d94f8bff106bceacb49b16613fd
vikas-git1 <vikasmewara.dev@gmail.com>
2025-08-12 09:50:03 +0530

    Add string.js file

---
+++ string.js
@@ -0,0 +1,21 @@
+//! A. Very Frequently Asked (Must Know)
+
+//     Reverse a string.
+
+//     Check if a string is a palindrome.
+
+//     Count vowels and consonants in a string.
+
+//     Count the occurrence of each character in a string.
+
+//     Remove duplicate characters from a string.
+
+//     Find the first non-repeating character in a string.
+
+//     Check if two strings are anagrams.
+
+//     Change the case of each character (uppercase ↔ lowercase).
+
+//     Find the longest word in a sentence.
+
+//     Check if a string contains only digits.
