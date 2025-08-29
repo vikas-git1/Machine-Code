7072f857c2d3cf6594ef59709613c6a849c5a047
vikas-git1 <vikasmewara.dev@gmail.com>
2025-08-29 08:13:17 +0530

    SOlved almost 50 questions

--- 50coding.js
+++ 50coding.js
@@ -305,36 +305,148 @@ const addNumStrs = (numStr1, numStr2) => {
 };
 // console.log(addNumStrs("100", "99")); //Not correct for large data
 
-// 31. Create a function that will return the number of words in a text
-// 32. Create a function that will capitalize the first letter of each word in a text
-// 33. Calculate the sum of numbers received in a comma delimited string
+// 31. Create a function that will return the number of words in a text.
+const countWordsInString = (str) => str.split(" ").length;
+// console.log(countWordsInString("This is coding round"));
+
+// 32. Create a function that will capitalize the first letter of each word in a text.
+const capitalizeFirstLetter = (str) => {
+  let strArr = str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+  return strArr;
+};
+// console.log(capitalizeFirstLetter("hellow world welcome to javascript"));
+
+// 33. Calculate the sum of numbers received in a comma delimited string.
+const sumNumStrComma = (str) => {
+  let sum = str
+    .split(",")
+    .map((num) => Number(num.trim()))
+    .reduce((total, curr) => total + curr, 0);
+  return `The sum of numbers in the given string is : ${sum}`;
+};
+// console.log(sumNumStrComma("12,                  35, 8"));
+
 // 34. Create a function that returns an array with words inside a text.
-// 35. Create a function to convert a CSV text to a “bi-dimensional” array
-// 36. Create a function that converts a string to an array of characters
-// 37. Create a function that will convert a string in an array containing the ASCII codes of
-// each character
-// 38. Create a function that will convert an array containing ASCII codes in a string
-// 39. Implement the Caesar cypher
-// 40. Implement the bubble sort algorithm for an array of numbers
-// 41. Create a function to calculate the distance between two points defined by their x, y
-// coordinates
-// 42. Create a function that will return a Boolean value indicating if two circles
-// defined by center coordinates and radius are intersecting
-// 43. Create a function that will receive a bi-dimensional array as argument and a
-// number and will extract as a unidimensional array the column specified by the
-// number
-// 44. Create a function that will convert a string containing a binary number into a
-// number
-// 45. Create a function to calculate the sum of all the numbers in a jagged array
-// (contains numbers or other arrays of numbers on an unlimited number of
-// levels)
-// 46. Find the maximum number in a jagged array of numbers or array of numbers
-// 47. Deep copy a jagged array with numbers or other arrays in a new array
-// 48. Create a function to return the longest word in a string
-// 49. Shuffle an array of strings
-// 50. Create a function that will receive n as argument and return an array of n
-// random numbers from 1 to n. The numbers should be unique inside the array.
-// 51. Find the frequency of letters inside a string. Return the result as an array of
-// arrays. Each subarray has 2 elements: letter and number of occurrences.
-// 52. Calculate Fibonacci(500) with high precision (all digits)
-// 53. Calculate 70! with high precision (all digits)
+const arrFromStr = (str) => str.trim().split(/\s+/);
+// console.log(arrFromStr("This is    array made from string      "));
+
+// 35. Create a function to convert a CSV text to a “bi-dimensional” array.
+const csvToArr = (str) => {
+  let strArr = str.split("\n").map((word) => word.split(","));
+  return strArr;
+};
+// console.log(csvToArr("name,age,city\nAlice,25,London\nBob,30,Paris"));
+
+// 36. Create a function that converts a string to an array of characters.
+const strToArr = (str) => str.trim().replace(/\s+/g, "").split("");
+// console.log(strToArr("         Vi              ka             s  "));
+
+// 37. Create a function that will convert a string in an array containing the ASCII codes of each character
+const asciiCodeOfChar = (str) => {
+  let codeArr = str
+    .trim()
+    .replace(/\s+/g, "")
+    .split("")
+    .map((char) => char.charCodeAt(0));
+  return codeArr;
+};
+// console.log(asciiCodeOfChar("vi  k  as   "));
+
+// 38. Create a function that will convert an array containing ASCII codes in a string.
+const asciiToStr = (arr) =>
+  arr.map((code) => String.fromCharCode(code)).join("");
+// console.log(asciiToStr([118, 105, 107, 97, 115]));
+
+// 39. Implement the Caesar cypher.
+const caesarCypher = (str, shift) => {};
+
+// 40. Implement the bubble sort algorithm for an array of numbers.
+const bubbleSort = (arr) => {
+  let newArr = arr.slice();
+  let length = newArr.length;
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = 0; j < length - 1; j++) {
+      if (newArr[j] > newArr[j + 1]) {
+        [newArr[j], newArr[j + 1]] = [newArr[j + 1], newArr[j]];
+      }
+    }
+  }
+  return newArr;
+};
+const arr40 = [2, 7, 5, 1, 8, 3, 9, 4, 6];
+// console.log(bubbleSort(arr40));
+
+// 41. Create a function to calculate the distance between two points defined by their x, y coordinates
+const calculateDistanceXY = (x1, y1, x2, y2) =>
+  Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+// console.log(calculateDistanceXY(10, 20, 100, 200));
+
+// 42. Create a function that will return a Boolean value indicating if two circles defined by center coordinates and radius are intersecting.
+const areIntersecting = () => {};
+
+// 43. Create a function that will receive a bi-dimensional array as argument and a number and will extract as a unidimensional array the column specified by the number.
+
+// 44. Create a function that will convert a string containing a binary number into a number.
+
+// 45. Create a function to calculate the sum of all the numbers in a jagged array (contains numbers or other arrays of numbers on an unlimited number of levels).
+const sumJaggedArr = (arr) => {
+  let sum = 0;
+  for (let el of arr) {
+    if (Array.isArray(el)) {
+      sum += sumJaggedArr(el);
+    } else {
+      sum += el;
+    }
+  }
+  return sum;
+};
+const arr45 = [10, [23, 12, [20, 2, 5], 1], 9, 67, [45, 43, [13]]];
+// console.log(sumJaggedArr(arr45));
+
+// 46. Find the maximum number in a jagged array of numbers or array of numbers.
+const findMaxNumInJaggedArr = (arr) => {
+  let maxNum = -Infinity;
+  for (let el of arr) {
+    if (Array.isArray(el)) {
+      let subMax = findMaxNumInJaggedArr(el);
+      if (maxNum < subMax) maxNum = subMax;
+    } else {
+      if (maxNum < el) maxNum = el;
+    }
+  }
+  return maxNum;
+};
+const arr46 = [10, [23, 12, [20, 2, 5], 1], 9, 67, [45, 43, [13]]];
+console.log(findMaxNumInJaggedArr(arr46));
+
+// 47. Deep copy a jagged array with numbers or other arrays in a new array.
+
+// 48. Create a function to return the longest word in a string.
+const returnLongestWord = (str) => {
+  let strArr = str.trim().split(" ");
+  let longestWord = strArr[0];
+  for (let word of strArr)
+    if (longestWord.length < word.length) longestWord = word;
+  return `The longest word in the given string is:"${longestWord}"`;
+};
+// console.log(returnLongestWord(" find the longest wordssss"));
+
+// 49. Shuffle an array of strings.
+const shuffleArrOfStr = (arr) => {
+  let newArr = [...arr];
+  for (let i = 0; i < newArr.length; i++) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[randomIndex]] = [newArr[randomIndex], newArr[i]];
+  }
+
+  return newArr;
+};
+const arr49 = ["shuffle", "this", "array", "randomly"];
+// console.log(shuffleArrOfStr(arr49));
+
+// 50. Create a function that will receive n as argument and return an array of n random numbers from 1 to n. The numbers should be unique inside the array.
+
+// 51. Find the frequency of letters inside a string. Return the result as an array of arrays. Each subarray has 2 elements: letter and number of occurrences.
